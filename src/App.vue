63fcdd7001756a703a8a9a55ea7350c305664187
Kelvin Melim <kelvinmelim@gmail.com>
2022-09-20 16:33:37 -0300

    1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <comp-sorteio></comp-sorteio>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import compSorteio from './components/compSorteio.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+    components: {
+      compSorteio
+}
 }
 </script>
 
